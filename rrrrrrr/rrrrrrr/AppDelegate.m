076940c78892e11687aeef0fb8bc6289d557ebbf
HomerLynn <784888121@qq.com>
2019-05-23 11:31:11 +0800

    RRRRRR

--- rrrrrrr/rrrrrrr/AppDelegate.m
+++ rrrrrrr/rrrrrrr/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"9999");
     return YES;
 }
 
